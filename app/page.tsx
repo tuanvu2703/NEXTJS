e0c011b06b9b2eb356b947f7740def5ede08c701
TuanVux <tuanvukg2703@gmail.com>
2026-03-12 15:20:24 +0700

    chapter14: Adding Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape} />
-      <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+      {/* <div className={styles.shape} />
+      <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" /> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
